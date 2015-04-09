2ee01804f589e8f3b15e9d4bca2c16d1b93c28f9
Fabio Rehm <fgrehm@gmail.com>
2015-04-08 22:54:37 -0300

    Playing with colliding balls

---
+++ Assets/Scripts/CurrentPlayerController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CurrentPlayerController : MonoBehaviour {
+	public GameObject whiteBall;
+
+	void Update () {
+		float z = 0.0f, x = 0.0f;
+
+		if (Input.GetKeyUp(KeyCode.UpArrow)) {
+			z = 1;
+		} else if (Input.GetKeyUp(KeyCode.DownArrow)) {
+			z = -1;
+		} else if (Input.GetKeyUp(KeyCode.RightArrow)) {
+			x = 1;
+		} else if (Input.GetKeyUp(KeyCode.LeftArrow)) {
+			x = -1;
+		}
+		int multiplier = 20;
+		Vector3 force = new Vector3(x * multiplier, 0.0f, z * multiplier);
+		whiteBall.rigidbody.AddForce(force, ForceMode.Impulse);
+	}
+}
