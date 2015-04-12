383ff9f76f11605b3763abea6147676782f56d56
Fabio Rehm <fgrehm@gmail.com>
2015-04-12 00:41:07 -0300

    Move camera along with cue ball

---
+++ Assets/Scripts/MainCameraController.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainCameraController : MonoBehaviour {
+	public GameObject cueBall;
+	private Vector3 offset;
+	
+	void Start () {
+		offset = transform.position - cueBall.transform.position;
+	}
+
+	void LateUpdate () {
+		transform.position = cueBall.transform.position + offset;
+	}
+}
