690d908237ee5ba9eb30af896cf49fb3ed003204
Fabio Rehm <fgrehm@gmail.com>
2015-04-12 17:55:13 -0300

    Shoot with Ctrl + Arrow and drop impulse force mode

--- Assets/Scripts/CurrentPlayerController.cs
+++ Assets/Scripts/CurrentPlayerController.cs
@@ -6,18 +6,20 @@ public class CurrentPlayerController : MonoBehaviour {
 	public int multiplier;
 
 	void Update () {
+		//Debug.Log (cueBall.rigidbody.IsSleeping ());
+		Debug.Log (Input.GetButton("Fire1"));
+		if (Input.GetButton("Fire1") && cueBall.rigidbody.IsSleeping()) {
+			Strike ();
+		}
+	}
+
+	void Strike() {
 		float z = 0.0f, x = 0.0f;
 
-		if (Input.GetKeyUp(KeyCode.UpArrow)) {
-			z = 1;
-		} else if (Input.GetKeyUp(KeyCode.DownArrow)) {
-			z = -1;
-		} else if (Input.GetKeyUp(KeyCode.RightArrow)) {
-			x = 1;
-		} else if (Input.GetKeyUp(KeyCode.LeftArrow)) {
-			x = -1;
-		}
+		x = Input.GetAxis("Horizontal");
+		z = Input.GetAxis("Vertical");
+	
 		Vector3 force = new Vector3(x * multiplier, 0.0f, z * multiplier);
-		cueBall.rigidbody.AddForce(force, ForceMode.Impulse);
+		cueBall.rigidbody.AddForce(force);
 	}
 }
