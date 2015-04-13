d696b723a51bf19ca55c3bd04a0502b1380f4f11
Fabio Rehm <fgrehm@gmail.com>
2015-04-13 01:21:14 -0300

    Trying to strike balls automagically (not very successful)

---
+++ Assets/Scripts/CueController.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CueController : MonoBehaviour {
+	public GameObject cueBall;
+
+	private Vector3 force;
+	private bool striking = true;
+	private Vector3 positionOffset;
+
+	void Start() {
+		positionOffset = transform.position - cueBall.transform.position;
+		force = Vector3.down * 1.1f;
+	}
+
+	void Update() {
+		if (striking)
+			transform.Translate(force);
+		else if (cueBall.rigidbody.IsSleeping()) {
+			transform.position = cueBall.transform.position + positionOffset;
+			transform.Translate(Vector3.up * 3f);
+			striking = true;
+		}
+	}
+
+	void OnCollisionEnter(Collision collision) {
+		Debug.Log ("Collision");
+		if (collision.gameObject != cueBall)
+			return;
+
+		striking = false;
+
+		transform.rigidbody.velocity = new Vector3();
+	}
+}
--- Assets/Scripts/CurrentPlayerController.cs
+++ Assets/Scripts/CurrentPlayerController.cs
@@ -3,24 +3,24 @@ using System.Collections;
 
 public class CurrentPlayerController : MonoBehaviour {
 	public GameObject cueBall;
-	public int multiplier;
+	public GameObject cue;
 
-	void Update () {
-		if (Input.GetButton("Fire1") && cueBall.rigidbody.IsSleeping()) {
-			Strike();
-		}
-	}
-
-	void Strike() {
-		float x = Input.GetAxis("Horizontal");
-		x = (float)System.Math.Ceiling(x);
-
-		float z = Input.GetAxis("Vertical");
-		z = (float)System.Math.Ceiling(z);
-
-		Debug.Log("x=" + x + " z="+z);
-
-		Vector3 force = new Vector3(x, 0.0f, z) * multiplier;
-		cueBall.rigidbody.AddForce(force, ForceMode.Acceleration);
-	}
+//	void Update () {
+//		if (Input.GetButton("Fire1") && cueBall.rigidbody.IsSleeping()) {
+//			Strike();
+//		}
+//	}
+//
+//	void Strike() {
+//		float x = Input.GetAxis("Horizontal");
+//		x = (float)System.Math.Ceiling(x);
+//
+//		float z = Input.GetAxis("Vertical");
+//		z = (float)System.Math.Ceiling(z);
+//
+//		Debug.Log("x=" + x + " z="+z);
+//
+//		Vector3 force = new Vector3(x, 0.0f, z) * multiplier;
+//		cueBall.rigidbody.AddForce(force, ForceMode.Acceleration);
+//	}
 }
