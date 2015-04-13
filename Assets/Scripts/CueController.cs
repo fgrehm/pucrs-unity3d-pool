fcff7502eb45fe62063c2aefb31e1f3a91a0a83f
Fabio Rehm <fgrehm@gmail.com>
2015-04-13 19:46:38 -0300

    Striking balls are starting to work a bit better

--- Assets/Scripts/CueController.cs
+++ Assets/Scripts/CueController.cs
@@ -3,6 +3,7 @@ using System.Collections;
 
 public class CueController : MonoBehaviour {
 	public GameObject cueBall;
+	public float velocity;
 
 	private Vector3 force;
 	private bool striking = true;
@@ -10,26 +11,30 @@ public class CueController : MonoBehaviour {
 
 	void Start() {
 		positionOffset = transform.position - cueBall.transform.position;
-		force = Vector3.down * 1.1f;
+		force = Vector3.down * velocity;
 	}
 
-	void Update() {
-		if (striking)
-			transform.Translate(force);
-		else if (cueBall.rigidbody.IsSleeping()) {
+	void FixedUpdate() {
+		if (striking) {
+			transform.Translate(force * Time.deltaTime);
+		} else if (cueBall.rigidbody.IsSleeping()) {
 			transform.position = cueBall.transform.position + positionOffset;
-			transform.Translate(Vector3.up * 3f);
+			transform.Translate(Vector3.up * velocity * 1.5f);
 			striking = true;
+			this.renderer.enabled = true;
+			this.collider.enabled = true;
 		}
 	}
 
-	void OnCollisionEnter(Collision collision) {
-		Debug.Log ("Collision");
-		if (collision.gameObject != cueBall)
+	void OnTriggerEnter(Collider collider) {
+		Debug.Log("Collision");
+		if (collider.gameObject != cueBall)
 			return;
 
 		striking = false;
-
-		transform.rigidbody.velocity = new Vector3();
+		var contactPoint = collider.ClosestPointOnBounds(this.collider.transform.position);
+		cueBall.rigidbody.AddForceAtPosition(Vector3.forward * 1000, contactPoint);
+		this.renderer.enabled = false;
+		this.collider.enabled = false;
 	}
 }
