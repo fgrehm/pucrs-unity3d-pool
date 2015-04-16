f78469055eccb35399e4ed2e9f95c6eb03ba9a17
Fabio Rehm <fgrehm@gmail.com>
2015-04-15 22:44:05 -0300

    Smarter collisions and striking

--- Assets/Scripts/CueController.cs
+++ Assets/Scripts/CueController.cs
@@ -6,15 +6,22 @@ public class CueController : MonoBehaviour {
 	public float velocity;
 
 	private Vector3 force;
-	private bool striking = true;
+	private bool striking = false;
 	private Vector3 positionOffset;
 
 	void Start() {
 		positionOffset = transform.position - cueBall.transform.position;
 		force = Vector3.down * velocity;
+		cueBall.rigidbody.sleepVelocity = 0f;
+		cueBall.rigidbody.sleepAngularVelocity = 0f;
 	}
 
 	void FixedUpdate() {
+//		Debug.Log(Time.frameCount + " sleeping " + cueBall.rigidbody.IsSleeping());
+//		Debug.Log(Time.frameCount + " Striking " + striking);
+//		Debug.Log(Time.frameCount + " velocit " + cueBall.rigidbody.velocity);
+//		Debug.Log(Time.frameCount + " ang velo " + cueBall.rigidbody.angularVelocity);
+
 		if (striking) {
 			transform.Translate(force * Time.deltaTime);
 		} else if (cueBall.rigidbody.IsSleeping()) {
@@ -26,14 +33,13 @@ public class CueController : MonoBehaviour {
 		}
 	}
 
-	void OnTriggerEnter(Collider collider) {
+	void OnCollisionEnter(Collision collision) {
 		Debug.Log("Collision");
-		if (collider.gameObject != cueBall)
+		if (collision.gameObject != cueBall)
 			return;
 
 		striking = false;
-		var contactPoint = collider.ClosestPointOnBounds(this.collider.transform.position);
-		cueBall.rigidbody.AddForceAtPosition(Vector3.forward * 1000, contactPoint);
+		cueBall.rigidbody.AddForceAtPosition(collision.contacts[0].normal * -1500, collision.contacts[0].point);
 		this.renderer.enabled = false;
 		this.collider.enabled = false;
 	}
