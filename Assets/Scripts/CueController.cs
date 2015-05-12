1284e00ddd2da226f443d2e0e152745512dcbe19
Fabio Rehm <fgrehm@gmail.com>
2015-05-12 10:14:37 -0300

    Striking balls plus some other stuff

--- Assets/Scripts/CueController.cs
+++ Assets/Scripts/CueController.cs
@@ -2,14 +2,43 @@
 using System.Collections;
 
 public class CueController : MonoBehaviour {
+	public class StrikeMessage {
+		public float StrikeSpeed { get; private set; }
+		public float ForceMultiplier { get; private set; }
+		public GameObject CueBall { get; private set; }
 
-	// Use this for initialization
+		public StrikeMessage(float strikeSpeed, float forceMultiplier, GameObject cueBall) {
+			StrikeSpeed = strikeSpeed;
+			ForceMultiplier = forceMultiplier;
+			CueBall = cueBall;
+		}
+	}
+
+	private StrikeMessage strike;
+
 	void Start () {
 	
 	}
+
+	void FixedUpdate () {
+		if (strike != null)
+			transform.Translate(strike.StrikeSpeed * Vector3.up * Time.deltaTime);
+	}
 	
-	// Update is called once per frame
-	void Update () {
-	
+	void OnCollisionEnter(Collision collision) {
+		if (strike == null || collision.gameObject != strike.CueBall)
+			return;
+
+		var lastStrike = strike;
+		strike = null;
+		lastStrike.CueBall.GetComponent<Rigidbody>().AddForceAtPosition(collision.contacts[0].normal * lastStrike.ForceMultiplier, collision.contacts[0].point);
+		//GetComponent<Renderer>().enabled = false;
+		GetComponent<Collider>().enabled = false;
+		foreach (var renderer in GetComponentsInChildren<Renderer>())
+			renderer.enabled = false;
+	}
+
+	void Strike(StrikeMessage strike) {
+		this.strike = strike;
 	}
 }
--- Assets/Scripts/CurrentPlayerController.cs
+++ Assets/Scripts/CurrentPlayerController.cs
@@ -2,14 +2,38 @@
 using System.Collections;
 
 public class CurrentPlayerController : MonoBehaviour {
+	public GameObject cue;
+	public GameObject cueBall;
+	public GameObject redBalls;
+	public float sleepThreshold;
 
-	// Use this for initialization
+	// TODO: This is going to be handled by the script instead of being passed in
+	public float strikeSpeed;
+	public float multiplier;
+
 	void Start () {
-	
+		cueBall.GetComponent<Rigidbody>().sleepThreshold = sleepThreshold;
+		foreach (var ballRigidBody in redBalls.GetComponentsInChildren<Rigidbody>()) 
+			ballRigidBody.sleepThreshold = sleepThreshold;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if (Input.GetButton("Fire1") && ballsAreSleeping) {
+			cue.SendMessage("Strike", new CueController.StrikeMessage(strikeSpeed, -multiplier, cueBall));
+		}
+	}
+
+	private bool ballsAreSleeping {
+		get {
+			if (!cueBall.GetComponent<Rigidbody>().IsSleeping()) 
+				return false;
+
+			foreach (var ballRigidBody in redBalls.GetComponentsInChildren<Rigidbody>())
+				if (!ballRigidBody.IsSleeping()) 
+					return false;
+
+			return true;
+		}
 	}
 }
