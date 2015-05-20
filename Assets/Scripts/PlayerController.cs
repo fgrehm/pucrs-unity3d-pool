f8ded23c8321bf630fade0c92fab30c8beec0045
Fabio Rehm <fgrehm@gmail.com>
2015-05-20 18:31:40 -0300

    Move cue along with white ball

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 
 public class PlayerController : MonoBehaviour {
+	public GameObject cue;
 	public GameObject cueBall;
 	public GameObject redBalls;
 	public GameObject mainCamera;
@@ -10,13 +11,20 @@ public class PlayerController : MonoBehaviour {
 
 	private Vector3 strikeDirection;
 	private Vector3 cameraOffset;
+	private Vector3 cueOffset;
 	private Quaternion cameraRotation;
+	private Quaternion cueRotation;
 
 	// Use this for initialization
 	void Start () {
 		strikeDirection = Vector3.forward;
+
 		cameraOffset = cueBall.transform.position - mainCamera.transform.position;
 		cameraRotation = mainCamera.transform.rotation;
+
+		cueOffset = cueBall.transform.position - cue.transform.position;
+		cueRotation = cue.transform.rotation;
+
 		cueBall.GetComponent<Rigidbody>().sleepThreshold = 10f;
 		foreach (var rigidbody in redBalls.GetComponentsInChildren<Rigidbody>())
 			rigidbody.sleepThreshold = 3f;
@@ -28,12 +36,19 @@ public class PlayerController : MonoBehaviour {
 
 		if (Input.GetButton("Fire1") && body.IsSleeping()) {
 			body.AddForce(strikeDirection * maxForce);
+			cue.GetComponent<Renderer>().enabled = false;
 		} else if (body.IsSleeping()) {
+			cue.GetComponent<Renderer>().enabled = true;
 			var x = Input.GetAxis("Horizontal");
 			if (x != 0) {
 				var angle = x * 75 * Time.deltaTime;
 				strikeDirection = Quaternion.AngleAxis(angle, Vector3.up) * strikeDirection;
 				mainCamera.transform.RotateAround(cueBall.transform.position, Vector3.up, angle);
+				cue.transform.RotateAround(cueBall.transform.position, Vector3.up, angle);
+
+				cueRotation = cue.transform.rotation;
+				cueOffset = cueBall.transform.position - cue.transform.position;
+
 				cameraRotation = mainCamera.transform.rotation;
 				cameraOffset = cueBall.transform.position - mainCamera.transform.position;
 			}
@@ -45,6 +60,9 @@ public class PlayerController : MonoBehaviour {
 		if (!cueBall.GetComponent<Rigidbody>().IsSleeping()) {
 			mainCamera.transform.position = cueBall.transform.position - cameraOffset;
 			mainCamera.transform.rotation = cameraRotation;
+
+			cue.transform.position = cueBall.transform.position - cueOffset;
+			cue.transform.rotation = cueRotation;
 		}
 	}
 }
