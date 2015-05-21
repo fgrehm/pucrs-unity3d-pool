735c039a816a299173abfd7c1f417e362c38007a
Fabio Rehm <fgrehm@gmail.com>
2015-05-21 09:50:51 -0300

    Prevent balls from jumping out of the table

---
+++ Assets/Scripts/SnookerBallController.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SnookerBallController : MonoBehaviour {
+	void FixedUpdate () {
+		var rigidbody = GetComponent<Rigidbody>();
+		if (rigidbody.velocity.y > 0) {
+			var velocity = rigidbody.velocity;
+			velocity.y *= 0.3f;
+			rigidbody.velocity = velocity;
+		}
+	}
+}
