03048e0c896cf2eeb528a96ba2c48372d08b8299
Fabio Rehm <fgrehm@gmail.com>
2015-05-21 19:08:04 -0300

    Basic cue animation (soon to be used by the strike controls)

--- Assets/Scripts/CueController.cs
+++ Assets/Scripts/CueController.cs
@@ -2,15 +2,15 @@
 using System.Collections;
 
 public class CueController : MonoBehaviour {
+	public GameObject cueBall;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+	private float cueDirection = 1;
+	private float speed = 7;
 
+	void FixedUpdate () {
+		var distance = Vector3.Distance(transform.position, cueBall.transform.position);
+		if (distance < 27.5 || distance > 36)
+			cueDirection *= -1;
+		transform.Translate(Vector3.down * speed * cueDirection * Time.fixedDeltaTime);
 	}
 }
