fddbfca2369649ce06c77635ce3dac1b759417fe
Fabio Rehm <fgrehm@gmail.com>
2015-05-21 19:19:27 -0300

    Balls can be pocketed!

---
+++ Assets/Scripts/PocketsController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PocketsController : MonoBehaviour {
+	public GameObject redBalls;
+	public GameObject cueBall;
+
+	void OnCollisionEnter(Collision collision) {
+		foreach (var transform in redBalls.GetComponentsInChildren<Transform>())
+			if (transform.name == collision.gameObject.name)
+				Debug.Log("Red ball dropped");
+
+		if (cueBall.transform.name == collision.gameObject.name)
+			Debug.Log("Cue ball dropped");
+
+		GameObject.Destroy(collision.gameObject);
+	}
+}
