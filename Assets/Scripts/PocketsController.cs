144da430744e63d7fee8eafee4665ffc0a173b5f
Fabio Rehm <fgrehm@gmail.com>
2015-05-24 17:20:50 -0300

    Do not destroy the cue ball, just move it back to the original position

--- Assets/Scripts/PocketsController.cs
+++ Assets/Scripts/PocketsController.cs
@@ -5,14 +5,24 @@ public class PocketsController : MonoBehaviour {
 	public GameObject redBalls;
 	public GameObject cueBall;
 
-	void OnCollisionEnter(Collision collision) {
-		foreach (var transform in redBalls.GetComponentsInChildren<Transform>())
-			if (transform.name == collision.gameObject.name)
-				Debug.Log("Red ball dropped");
+	private Vector3 originalCueBallPosition;
 
-		if (cueBall.transform.name == collision.gameObject.name)
+	void Start() {
+		originalCueBallPosition = cueBall.transform.position;
+	}
+
+	void OnCollisionEnter(Collision collision) {
+		foreach (var transform in redBalls.GetComponentsInChildren<Transform>()) {
+			if (transform.name == collision.gameObject.name) {
+				Debug.Log("Red ball dropped");
+				GameObject.Destroy(collision.gameObject);
+			}
+		}
+
+		if (cueBall.transform.name == collision.gameObject.name) {
 			Debug.Log("Cue ball dropped");
 
-		GameObject.Destroy(collision.gameObject);
+			cueBall.transform.position = originalCueBallPosition;
+		}
 	}
 }
