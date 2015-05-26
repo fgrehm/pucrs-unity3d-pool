6e764abd9c85eeea0ddd241ff38030f68f10109a
Fabio Rehm <fgrehm@gmail.com>
2015-05-26 01:01:10 -0300

    Render proper score on screen

---
+++ Assets/Scripts/ScoreController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+
+public class ScoreController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		var text = GetComponent<UnityEngine.UI.Text>();
+		var currentPlayer = PoolGameController.GameInstance.CurrentPlayer;
+		var otherPlayer = PoolGameController.GameInstance.OtherPlayer;
+		text.text = String.Format("* {0} - {1}\n{2} - {3}", currentPlayer.Name, currentPlayer.Points, otherPlayer.Name, otherPlayer.Points);
+	}
+}
